53e68695319bf61764205bc5b79c3bed3d695611
Srujansujji1717 <puttasrujan20bsc74@iiitkottayam.ac.in>
2022-06-14 11:33:11 +0530

    updated

--- Cascade and inheritance/main.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('#html').addEventListener('click', e => {
-    document.documentElement.classList.toggle('class-on-root');
-  });
-  
-  document.querySelector('#js').addEventListener('click', e => {
-    //most specific, automatic win
-    document.querySelector('li:nth-of-type(3)').style.setProperty('--color', 'purple');
-  });
